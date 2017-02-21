08b744c9de0de2d77862d9d9cfd681370a27e349
Masanobu Mizutani <James@Masanobus-MacBook-Air.local>
2017-02-21 09:58:20 +0900

    topからgame詳細へのリンク追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :find_team
+
+  private
+
+	def find_team
+		# current_userを取得するまで
+		@team ||= Team.find(Team::PENGUINS)
+	end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,4 @@
 class TeamsController < ApplicationController
-	before_action :find_team, only: %i(index)
-
   def index
   end
-
-	private
-
-	def find_team
-		# current_userを取得するまで
-		@team ||= Team.find(Team::PENGUINS)
-	end
 end
